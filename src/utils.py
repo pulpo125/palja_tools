e518c096824c737747abdcad89c0f4cb3bda6e02
pulpo125 <kang.pulpo@gmail.com>
2025-05-17 19:14:48 +0900

    ClientManager 개발, get_openai_chat_llm_client 함수로 변경

--- src/utils.py
+++ src/utils.py
@@ -3,11 +3,27 @@ from langchain_openai import ChatOpenAI
 from src.config import cfg
 
 
-def get_openai_chat_llm():
-    llm = ChatOpenAI(
-        model=cfg.openai.llm_model,
-        max_tokens=cfg.openai.llm_max_tokens,
-        api_key=cfg.openai.api_key,
-        temperature=0,
-    )
-    return llm
+# =========================================================
+# Clients
+# =========================================================
+class ClientManager:
+    def __new__(cls):
+        if not hasattr(cls, "instance"):
+            cls.instance = super(ClientManager, cls).__new__(cls)
+        return cls.instance
+
+
+client_manager = ClientManager()
+
+
+def get_openai_chat_llm_client():
+    key = "ChatOpenAI"
+    if not hasattr(client_manager, key):
+        chat_llm = ChatOpenAI(
+            model=cfg.openai.llm_model,
+            max_tokens=cfg.openai.llm_max_tokens,
+            api_key=cfg.openai.api_key,
+            temperature=0,
+        )
+        setattr(client_manager, key, chat_llm)
+    return getattr(client_manager, key)
