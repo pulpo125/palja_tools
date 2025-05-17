648e64fe22979818d2f9db497d1d28fa3db87331
pulpo125 <kang.pulpo@gmail.com>
2025-05-17 19:20:42 +0900

    주석 추가

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,11 @@ from src.config import cfg
 # Clients
 # =========================================================
 class ClientManager:
+    """
+    A singleton class to manage llm clients.
+    'instance' 속성이 없으면 새 인스턴스를 생성, 있으면 동일한 인스턴스를 반환
+    """
+
     def __new__(cls):
         if not hasattr(cls, "instance"):
             cls.instance = super(ClientManager, cls).__new__(cls)
@@ -25,5 +30,5 @@ def get_openai_chat_llm_client():
             api_key=cfg.openai.api_key,
             temperature=0,
         )
-        setattr(client_manager, key, chat_llm)
+        setattr(client_manager, key, chat_llm)  # 생성한 인스턴스를 속성으로 저장
     return getattr(client_manager, key)
